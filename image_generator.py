d35d05799dcdff5aeaac17ea6b9c30e0beb0a6d3
mYNamEs7 <yura97078@gmail.com>
2026-01-19 13:30:53 +0300

    init project

---
+++ image_generator.py
@@ -0,0 +1,59 @@
+from PIL import Image
+from diffusers import StableDiffusionPipeline, DiffusionPipeline
+import torch
+from diffusers.loaders import FromSingleFileMixin
+
+reference_img = Image.open("result.png").convert("RGB")
+
+def init():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("Using:", device)
+
+    model_path = r"D:\Program Files\Local AI\abyssorangemix2SFW_abyssorangemix2Sfw.safetensors"
+    model_path = r"D:\Program Files\Local AI\meinamix_v12Final.safetensors"
+    pipe = StableDiffusionPipeline.from_single_file(
+        model_path,
+        torch_dtype=torch.float16,
+    )
+    # pipe = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+    # pipe.load_lora_weights(r"D:\Program Files\Local AI\meinamix_v12Final.safetensors", weight=0.8)
+    # pipe.load_lora_weights(r"D:\Program Files\Local AI\easynegative.safetensors", weight=0.7)
+    # pipe.load_lora_weights(r"D:\Program Files\Local AI\Loraeyes_V1.safetensors", weight=0.001)
+    pipe.safety_checker = None
+    pipe.requires_safety_checker = False
+    pipe.to(device)
+    return pipe
+
+negative = "loli, child, bad anatomy, deformed, extra limbs"
+
+
+def generate(pipe, prompt: str):
+    image = pipe(
+        prompt=prompt,
+        negative_prompt=negative,
+        num_inference_steps=30,
+        guidance_scale=7.5,
+    ).images[0]
+
+    global reference_img
+    reference_img = image
+
+    return image
+
+def generate_with_reference(pipe, prompt, strength=0):
+    image = pipe(
+        prompt=prompt,
+        negative_prompt=negative,
+        image=Image.open("result.png").convert("RGB"),
+        strength=strength,  # насколько сильно менять сцену, 0.3–0.5 обычно ок
+        num_inference_steps=30,
+        guidance_scale=7.5,
+    ).images[0]
+    return image
+
+# marin_prompt = "Marin Kitagawa in anime style, adult, mature face, long blonde hair, wearing her signature outfit, standing, confident pose, detailed anime style, high quality, 1girl, best quality, masterpiece, very aesthetic, realistic proportions, soft lighting"
+# neg = "loli, child, bad anatomy, deformed, extra limbs"
+# pipe1 = init()
+# img = generate(pipe1, "Marin Kitagawa")
+# img.show()
+# img.save("result.png")
---
+++ main.py
@@ -0,0 +1,78 @@
+import asyncio
+import logging
+import sys
+from image_generator import generate, init, generate_with_reference, reference_img
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message, FSInputFile, BotCommand
+
+is_chat = False
+
+TOKEN = "7997799086:AAEJ5980N9jG2MOWPSSLMyMGmSLGdH16Mq0"
+GENERATED_IMAGE_PATH = "result.png"
+dp = Dispatcher()
+
+pipe = init()
+
+def generate_image(prompt: str):
+    if is_chat:
+        img = generate(pipe, prompt)
+        img.save(GENERATED_IMAGE_PATH)
+    else:
+        img = generate_with_reference(pipe, prompt)
+        img.save(GENERATED_IMAGE_PATH)
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message):
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+@dp.message(Command("img"))
+async def send_photo_handler(message: Message):
+    try:
+        prompt = message.text.removeprefix("/img ")
+        await message.reply("Получен запрос.\nГенерируем изображение...")
+
+        generate_image(prompt)
+        await message.answer_photo(photo=FSInputFile(GENERATED_IMAGE_PATH))
+    except Exception as e:
+        await message.reply(f"Произошла ошибка при генерации изображения: {str(e)}")
+
+@dp.message(Command("chat_with"))
+async def chat_with_image_handler(message: Message):
+    await message.answer("Принято! Жду последующих указаний с данным персонажем")
+    global is_chat
+    is_chat = True
+
+@dp.message(Command("off_chat"))
+async def chat_with_image_handler(message: Message):
+    await message.answer("Принято! Жду последующих указаний с данным персонажем")
+    global is_chat
+    is_chat = False
+
+# @dp.message(Command("iter_count"))
+# async def send_photo_handler(message: Message):
+#     try:
+#         pipe.num_inference_steps = int(message.text.removeprefix("/iter_count "))
+#     except Exception as e:
+#         await message.reply(f"Произошла ошибка: {str(e)}")
+
+
+async def main():
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await bot.set_my_commands([
+        BotCommand(command="start", description="Запустить бота"),
+        BotCommand(command="img", description="Сгенерировать изображение"),
+        BotCommand(command="chat_with", description="Чат с персонажем"),
+        BotCommand(command="off_chat", description="Чат с персонажем"),
+        # BotCommand(command="iter_count", description="Количество итераций при генерации изображения"),
+    ])
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
